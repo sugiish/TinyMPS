8a7cd768c7e945032d70c8b6addf5b18d4ac1431
杉原祥太 <sugihara@Shota-SUGIHARA-MacBook.local>
2017-07-23 23:24:03 +0900

    implemented neighbor search

--- include/grid.h
+++ include/grid.h
@@ -1,7 +1,9 @@
 #ifndef MPS_GRID_H_INCLUDED
 #define MPS_GRID_H_INCLUDED
 
+#include <unordered_map>
 #include <vector>
+
 #include <Eigen/Dense>
 
 using namespace Eigen;
@@ -17,21 +19,41 @@ public:
 	Grid(const Particles& particles, double grid_width);
 	virtual ~Grid();
 
+	void getNeighbor(int hash, int& begin, int& end);
+	
+	inline void getGridHash(std::vector<std::pair<int, int> >& ghash)
+	{
+		ghash = grid_hash;
+	}
+
+	void resetHash();
+	int getHashValue(const VectorXd& position) const;
+
+	inline int indexToHashValue(int index_x, int index_y)
+	{
+		return index_x + index_y * grid_number[0];
+	}
+
+	inline int indexToHashValue(int index_x, int index_y, int index_z)
+	{
+		return index_x + index_y * grid_number[0] + index_z * grid_number[1] * grid_number[0];
+	}
+
+
 private:
 	const Particles& particles;
 
 	VectorXd lower_bounds;
 	VectorXd higher_bounds;
 
-	std::vector<std::pair<int, int> > hash;
-	std::vector<int> index;
-
 	double grid_width;
 	int grid_number[3];
 
-	void resetHash();
-	int getHashValue(const VectorXd& position) const;
-
+	// hash, index
+	std::vector<std::pair<int, int> > grid_hash;
+	
+	// index, begin(order) end(order)
+	std::unordered_map<int, std::pair<int, int> > begin_hash;
 };
 
 
--- src/grid.cpp
+++ src/grid.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <vector>
 
 Grid::Grid(const Particles& particles, double grid_width) : particles(particles), grid_width(grid_width)
 {
@@ -25,21 +24,59 @@ Grid::Grid(const Particles& particles, double grid_width) : particles(particles)
 
 Grid::~Grid()
 {
+	begin_hash.clear();
+}
+
+void
+Grid::getNeighbor(int hash, int& begin, int& end)
+{
+	if(begin_hash.empty())
+	{
+		begin = -1; end = -1; return;
+	}
+
+	if(begin_hash.find(hash) == begin_hash.end())
+	{
+		begin = -1; end = -1; return;
+	}
+
+	begin = begin_hash[hash].first;
+	end = begin_hash[hash].second;
 
 }
 
 void
 Grid::resetHash()
 {
+	if(!begin_hash.empty()) begin_hash.clear();
+
 	int pt_num = particles.getParticlesNumber();
-	hash.resize(pt_num);
-	
+	if(pt_num == 0) return;
+
+	grid_hash.resize(pt_num);
 	for(int i = 0; i < pt_num; i++)
 	{
-		hash[i] = std::make_pair(getHashValue(particles.position.row(i)), i);
+		grid_hash[i] = std::make_pair(getHashValue(particles.position.row(i)), i);
 	}
 
-	std::sort(hash.begin(), hash.end());
+	std::sort(grid_hash.begin(), grid_hash.end());
+
+	int start_i = 0;
+	int start_value = grid_hash[0].first;
+	for(int i = 1; i < pt_num; i++)
+	{
+		if(start_value != grid_hash[i].first)
+		{
+			begin_hash[start_value] = std::make_pair(start_i, i - 1);
+			start_i = i;
+			start_value = grid_hash[i].first;
+		}
+
+		if(i == pt_num - 1)
+		{
+			begin_hash[start_value] = std::make_pair(start_i, i);
+		}
+	}
 }
 
 int
