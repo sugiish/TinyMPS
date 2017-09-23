32190c347c4bc5e224b77fcdd804bd9ca2c51c2e
s6s3 <z.zz.zzz.last.night@gmail.com>
2017-09-23 16:37:42 +0900

    Modified cavitation analysis

---
+++ examples/cavitation_analysis.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 Shota SUGIHARA
+// Distributed under the MIT License.
+#include <iostream>
+#include <boost/format.hpp>
+#include <Eigen/Dense>
+#include "condition.h"
+#include "grid.h"
+#include "particles.h"
+
+// Sample code using TinyMPS library.
+int main() {
+    tiny_mps::Condition condition("./input/input_cavitation.data");
+    tiny_mps::Particles particles("./input/input.grid", condition);
+    tiny_mps::Timer timer(condition);
+    Eigen::Vector3d minpos(-0.1, -0.1, 0);
+    Eigen::Vector3d maxpos(1.1, 2.1, 0);
+    while(particles.nextLoop("./output/output_%1%.vtk", timer)) {
+        particles.calculateTemporaryVelocity(condition.gravity, timer);
+        particles.updateTemporaryPosition(timer);
+        particles.giveCollisionRepulsionForce();
+        particles.updateTemporaryPosition(timer);
+        particles.calculateTemporaryParticleNumberDensity();
+        particles.checkSurfaceParticlesWithTanakaMasunaga();
+        particles.solvePressurePoissionWithTanakaMasunaga(timer);
+        particles.correctVelocity(timer);
+        particles.updateTemporaryPosition(timer);
+        particles.updateVelocityAndPosition();
+        particles.removeOutsideParticles(minpos, maxpos);
+    }
+}
