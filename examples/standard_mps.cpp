8f25fd6ef87c0d44acb437423bd6cf43175cf387
杉原祥太 <z.zz.zzz.last.night@gmail.com>
2017-10-06 16:46:09 +0900

    Add argv, argc into main().

--- examples/standard_mps.cpp
+++ examples/standard_mps.cpp
@@ -8,15 +8,21 @@
 #include "particles.h"
 
 // Sample code using TinyMPS library.
-int main() {
+int main(int argc, char* argv[]) {
   try {
-    tiny_mps::Condition condition("./input/input.data");
-    tiny_mps::Particles particles2("./input/dam.grid", condition);
-    tiny_mps::Particles particles = particles2;
+    std::string output_path = "./output/";
+    std::string input_data = "./input/input.data";
+    std::string input_grid = "./input/dam.grid";
+    if (argc >= 2) output_path = argv[1];
+    if (argc >= 3) input_data = argv[2];
+    if (argc >= 4) input_grid = argv[3];
+    output_path += "output_%1%.vtk";
+    tiny_mps::Condition condition(input_data);
+    tiny_mps::Particles particles(input_grid, condition);
     tiny_mps::Timer timer(condition);
     Eigen::Vector3d minpos(-0.1, -0.1, 0);
     Eigen::Vector3d maxpos(1.1, 2.1, 0);
-    while(particles.nextLoop("./output/output_%1%.vtk", timer)) {
+    while(particles.nextLoop(output_path, timer)) {
       particles.calculateTemporaryVelocity(condition.gravity, timer);
       particles.updateTemporaryPosition(timer);
       particles.giveCollisionRepulsionForce();
--- examples/tanaka_masunaga.cpp
+++ examples/tanaka_masunaga.cpp
@@ -8,22 +8,36 @@
 #include "particles.h"
 
 // Sample code using TinyMPS library.
-int main() {
-    tiny_mps::Condition condition("./input/input.data");
-    tiny_mps::Particles particles("./input/dam_tm.grid", condition);
+int main(int argc, char* argv[]) {
+  try {
+    std::string output_path = "./output/";
+    std::string input_data = "./input/input.data";
+    std::string input_grid = "./input/dam.grid";
+    if (argc >= 2) output_path = argv[1];
+    if (argc >= 3) input_data = argv[2];
+    if (argc >= 4) input_grid = argv[3];
+    output_path += "output_%1%.vtk";
+    tiny_mps::Condition condition(input_data);
+    tiny_mps::Particles particles(input_grid, condition);
     tiny_mps::Timer timer(condition);
     Eigen::Vector3d minpos(-0.1, -0.1, 0);
     Eigen::Vector3d maxpos(1.1, 2.1, 0);
-    while(particles.nextLoop("./output/output_%1%.vtk", timer)) {
-        particles.giveCollisionRepulsionForce();
-        particles.updateTemporaryPosition(timer);
-        particles.calculateTemporaryVelocity(condition.gravity, timer);
-        particles.calculateTemporaryParticleNumberDensity();
-        particles.checkSurfaceParticles();
-        particles.solvePressurePoissionTanakaMasunaga(timer);
-        particles.correctTanakaMasunagaVelocity(timer);
-        particles.updateTemporaryPosition(timer);
-        particles.updateVelocityAndPosition();
-        particles.removeOutsideParticles(minpos, maxpos);
+    while(particles.nextLoop(output_path, timer)) {
+      particles.giveCollisionRepulsionForce();
+      particles.updateTemporaryPosition(timer);
+      particles.calculateTemporaryVelocity(condition.gravity, timer);
+      particles.calculateTemporaryParticleNumberDensity();
+      particles.checkSurfaceParticles();
+      particles.solvePressurePoissionTanakaMasunaga(timer);
+      particles.correctTanakaMasunagaVelocity(timer);
+      particles.updateTemporaryPosition(timer);
+      particles.updateVelocityAndPosition();
+      particles.removeOutsideParticles(minpos, maxpos);
     }
+    return 0;
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  return 1;
 }
